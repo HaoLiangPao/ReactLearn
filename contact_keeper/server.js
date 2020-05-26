31d7292e202d1bce9f5892bbc1e29e6b72762f93
HaoLiangPao <hflsforeverhao@gmail.com>
2020-05-26 13:31:02 -0400

    Start the second react project, will use MongoDB, React and Express.

---
+++ contact_keeper/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+// Default Route
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome tho the ContactKeeper API ..." })
+);
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+// Specify the port used in the development environment
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}...`));
