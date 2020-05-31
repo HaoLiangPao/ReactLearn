3c765317428901b6ab2af26ec48441369c25e1a4
HaoLiangPao <hflsforeverhao@gmail.com>
2020-05-30 23:14:37 -0400

    Prepare for deploy

--- contact_keeper/server.js
+++ contact_keeper/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -17,6 +18,16 @@ app.get("/", (req, res) =>
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assests (React) in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  // Get anything except the three 'Defined Routes above' ??
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 // Specify the port used in the development environment
 const PORT = process.env.PORT || 5000;
